66e05b4957dee75b5d3faad8f28a54fe09844126
Mohammed Mirsal <dev.marcello14@gmail.com>
2022-03-04 17:08:27 +0300

    refactor: preparing project for localization.

--- Pods/Target Support Files/Pods-NetworkingServices-NetworkingServicesTests/Pods-NetworkingServices-NetworkingServicesTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetworkingServices_NetworkingServicesTests : NSObject
-@end
-@implementation PodsDummy_Pods_NetworkingServices_NetworkingServicesTests
-@end
--- Pods/Target Support Files/Pods-NetworkingServices/Pods-NetworkingServices-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetworkingServices : NSObject
-@end
-@implementation PodsDummy_Pods_NetworkingServices
-@end
